cbf9c0cf1abb4ce9e77be0404ea29efc595c3575
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-12-05 23:04:32 -0600

    Add MainLayout, Navabar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+	  extend: {
+	  },
 	},
-	plugins: [],
-}
+	daisyui: {
+	  themes: [
+		{
+		  mytheme: {
+			primary: "#d4af37",
+			secondary: "#f5f5f5",
+			accent: "#000225",
+			neutral: "#0e0503",
+			"base-100": "#1a1a1a",
+			info: "#00bde7",
+			success: "#00d6b0",
+			warning: "#fde047",
+			error: "#ea0032",
+		  },
+		  secondary: {
+			primary: "#d4af37",
+			secondary: "#1a1a1a",
+			accent: "#000225",
+			neutral: "#0e0503",
+			"base-100": "#f5f5f5",
+			info: "#00bde7",
+			success: "#00d6b0",
+			warning: "#fde047",
+			error: "#ea0032",
+		  },
+		},
+	  ],
+	},
+	plugins: [require("daisyui")],
+  };
+  
